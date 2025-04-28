2e79843dcedf319b53cd684d56377248317ce7bf
Florian Mangin <manginf54@gmail.com>
2025-04-28 14:12:08 +0200

    🎨 style: remove unnecessary blank lines

--- Back/src/main/java/com/presteo/app/PresteoApplication.java
+++ Back/src/main/java/com/presteo/app/PresteoApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PresteoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PresteoApplication.class, args);
     }
-
 }
